ddb809cdbcb9de7206d12f4eb82a19bee8ffa3e9
signalarun <signalarun@gmail.com>
2021-12-31 14:36:09 +0530

    new import

---
+++ sample/PythonUdpSocket/MyClient.py
@@ -0,0 +1,18 @@
+# Client to demonstate UDP communication
+import socket
+
+SERVER_IP = "127.0.0.1"
+SERVER_PORT = 7010
+SERVER_ADDRESS   = (SERVER_IP, SERVER_PORT)
+BUFFER_SIZE = 1024
+
+msgFromClient       = "Hello Server"
+bytesToSend         = str.encode(msgFromClient)
+
+UDPClientSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+
+UDPClientSocket.sendto(bytesToSend, SERVER_ADDRESS)
+msgFromServer = UDPClientSocket.recvfrom(BUFFER_SIZE)
+
+msg = "Server{}: {}".format(msgFromServer[1], msgFromServer[0])
+print(msg)
---
+++ sample/PythonUdpSocket/MyServer.py
@@ -0,0 +1,26 @@
+# Server to demonstate UDP communication
+import socket
+
+LOCAL_IP     = "127.0.0.1" # Server IP
+LOCAL_PORT   = 7010 # Server port
+BUFFER_SIZE  = 1024 
+
+msgFromServer       = "Hello Client"
+bytesToSend         = str.encode(msgFromServer)
+
+UDPServerSocket = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
+UDPServerSocket.bind((LOCAL_IP, LOCAL_PORT))
+
+print("Server(UDP) up and listening")
+
+while(True):
+
+    bytesAddressPair = UDPServerSocket.recvfrom(BUFFER_SIZE)
+    message = bytesAddressPair[0]
+    address = bytesAddressPair[1]
+
+    clientMsg = "{}".format(message)
+    clientIP  = "Client{}".format(address)
+    print(clientIP + ": " + clientMsg)
+
+    UDPServerSocket.sendto(bytesToSend, address)
